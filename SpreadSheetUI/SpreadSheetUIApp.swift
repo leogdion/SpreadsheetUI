738fbe4f05cdcbfc1dfd624ff2b9deb45322412f
Leo Dion <leogdion@brightdigit.com>
2021-08-14 20:05:34 -0400

    Initial Commit

---
+++ SpreadSheetUI/SpreadSheetUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpreadSheetUIApp.swift
+//  SpreadSheetUI
+//
+//  Created by Leo Dion on 8/14/21.
+//
+
+import SwiftUI
+
+@main
+struct SpreadSheetUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
